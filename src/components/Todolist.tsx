b6bc191ea06d9195c6a15fccc24661ba18960248
Vova <podolian@gmail.com>
2023-09-03 18:46:27 +0300

    new

---
+++ src/components/Todolist.tsx
@@ -0,0 +1,17 @@
+
+
+export function Todolist() {
+	return (
+		<div>
+			<h1>learn</h1>
+			<input type="text" />
+			<button>+</button>
+			<ul>
+				<li><input type="checkbox" />react</li>
+				<li><input type="checkbox" />js</li>
+				<li><input type="checkbox" />css</li>
+				<li><input type="checkbox" />redux</li>
+			</ul>
+		</div>
+	)
+}
